c5d467e2c4fe392721be83470ce7c7c73df73e50
祐介 戸田 <k022g0025@m.neec.ac.jp>
2023-05-12 12:19:38 +0900

    01_00まで実装

--- Matrix4x4Func.h
+++ Matrix4x4Func.h
@@ -142,6 +142,11 @@ Matrix4x4 Transpose(Matrix4x4 matrix) {
 
 #pragma endregion
 
+
+#pragma region 行列作成系
+
+#pragma region アフィン行列
+
 // 平行移動行列
 Matrix4x4 MakeTranslateMatrix(const Vector3& translate) {
 	Matrix4x4 result{};
@@ -223,6 +228,59 @@ Matrix4x4 MakeAffineMatrix(const Vector3& scale, const Vector3& rotate, Vector3&
 	return Multiply(result, t);
 }
 
+#pragma endregion
+
+#pragma region レンダリングパイプライン
+
+// 透視射影行列
+Matrix4x4 MakePerspectiveFovMatrix(float fovY, float aspectRatio, float nearClip, float farClip) {
+	Matrix4x4 result{};
+
+	result.m[0][0] = (1.0f / aspectRatio) * (1.0f / std::tan(fovY / 2.0f));
+	
+	result.m[1][1] = (1.0f / std::tan(fovY / 2.0f));
+
+	result.m[2][2] = farClip / (farClip - nearClip);
+	result.m[2][3] = 1;
+
+	result.m[3][2] = (-nearClip * farClip) / (farClip - nearClip);
+
+	return result;
+}
+// 正射影行列
+Matrix4x4 MakeOrthographicMatrix(float left, float top, float right, float bottom, float nearClip, float farClip) {
+	Matrix4x4 result{};
+
+	result.m[0][0] = 2.0f / (right - left);
+	result.m[1][1] = 2.0f / (top - bottom);
+	result.m[2][2] = 2.0f / (farClip - nearClip);
+
+	result.m[3][0] = (left + right) / (left - right);
+	result.m[3][1] = (top + bottom) / (bottom - top);
+	result.m[3][2] = nearClip / (nearClip - farClip);
+	result.m[3][3] = 1;
+
+	return result;
+}
+// ビューポート変換行列
+Matrix4x4 MakeViewportMatrix(float left, float top, float width, float height, float minDepth, float maxDepth) {
+	Matrix4x4 result{};
+
+	result.m[0][0] = width / 2.0f;
+	result.m[1][1] = -(height / 2.0f);
+	result.m[2][2] = maxDepth - minDepth;
+
+	result.m[3][0] = left + (width / 2.0f);
+	result.m[3][1] = top + (height / 2.0f);
+	result.m[3][2] = minDepth;
+	result.m[3][3] = 1;
+
+	return result;
+}
+
+#pragma endregion
+
+#pragma endregion
 
 
 // 座標変換
